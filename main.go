374de1d83ef1fa30ad41f9f9d37c0d18a93f7790
Csaba Palfi <csaba@palfi.me>
2017-04-12 09:35:00 +0200

    add -v flag

--- main.go
+++ main.go
@@ -34,14 +34,16 @@ func buildPageSpeedUrl(targetUrl string, strategy string) string {
 	return pageSpeedUrl.String()
 }
 
-func getScore(targetUrl string, strategy string) (error, int) {
+func getScore(targetUrl string, strategy string, verbose bool) (error, int) {
 	pageSpeedUrl := buildPageSpeedUrl(targetUrl, strategy)
 	response, err := myClient.Get(pageSpeedUrl)
 	if err != nil {
 		return err, 0
 	}
 	defer response.Body.Close()
-	debug((httputil.DumpResponse(response, true)))
+	if verbose {
+		debug((httputil.DumpResponse(response, true)))
+	}
 
 	result := &PageSpeedResult{}
 	json.NewDecoder(response.Body).Decode(result)
@@ -57,12 +59,13 @@ type PageSpeedResult struct {
 }
 
 func main() {
+	verbose := flag.Bool("v", false, "verbose")
 	flag.Parse()
 	var args = flag.Args()
 	targetUrl, slackChannel := args[0], args[1]
 
-	_, mobile := getScore(targetUrl, "mobile")
-	_, desktop := getScore(targetUrl, "desktop")
+	_, mobile := getScore(targetUrl, "mobile", *verbose)
+	_, desktop := getScore(targetUrl, "desktop", *verbose)
 
 	message := fmt.Sprintf("📱 %d  🖥 %d", mobile, desktop)
 
